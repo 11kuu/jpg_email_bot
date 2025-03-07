22a9cad8dc742af33d4145ba92e3e988b9b34a05
Lincoln <lincolndiscord17@gmail.com>
2025-03-07 08:21:26 -0400

    changes again

--- send_email.py
+++ send_email.py
@@ -19,7 +19,6 @@ plt.savefig('BRAZIL_BANK.png')
 
 send = str(input('enter the email you will send [ to_send_domain@domain.com ] :')).lower()
 receive = str(input('enter the email you will receive [ your_domain@domain.com ] :')).lower()
-
 email = 'your_email@domain.com'
 
 with open('C:/Users/linco/Documents/Visual Studio Projects/csv_email/password.txt') as f:
@@ -37,8 +36,6 @@ msg.set_content(f'Here is the graph plotted from Banco do Brasil :')
 with open('BRAZIL_BANK.png', 'rb') as content_file :
     content = content_file.read()
     msg.add_attachment(content, maintype = 'application', subtype = 'png', filename = 'BRAZIL_BANK.png')
-
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-
         smtp.login(email, senha_do_email)
         smtp.send_message(msg)
